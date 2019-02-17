28158e36abe23763ec42282aa422b6add84a4a3e
byrrice <32532726+byrrice@users.noreply.github.com>
2019-02-16 21:52:46 -0500

    Added tabs

--- App.js
+++ App.js
@@ -1,69 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { MapView } from 'expo';
+import { MapView, Icon } from 'expo';
+import {Ionicons} from '@expo/vector-icons'
 import Map from './components/Map'
-import SetMarker from './components/SetMarker'
+import Login from './components/Login'
+import {createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
-export default class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.setState = this.setState.bind(this);
-    this.state = {
-      // region: {
-      //   latitude: 41.5075,
-      //   longitude: -81.60844,
-      //   latitudeDelta: 0.007,
-      //   longitudeDelta: 0.019,
-      // },
-      markers: [{
-        latlng: {
-          latitude: 41.5075,
-          longitude: -81.60844,
+
+
+
+const TabNavigator = createBottomTabNavigator({
+  Home: {
+    screen: Map,
+    navigationOptions: {
+            tabBarIcon: ({ focused, tintColor }) => {
+                const iconName = `ios-home${focused ? '' : '-outline'}`;
+                return <Ionicons name={iconName} size={25} color={tintColor} />;
+            },
         },
-        title: 'marker test',
-        description: 'ta ta ra',
-      }],
-    }
-  }
-  getInitialState() {
-    return {
-        region: {
-        latitude: 41.5075,
-        longitude: -81.60844,
-        latitudeDelta: 0.007,
-        longitudeDelta: 0.019,
-      },
-    };
-  }
-  // What the fuck is this error
-  // onRegionChange(e) {
-  //   this.setState( {region: e} );
-  // }
+  },
+  Settings: {
+    screen: Login,
+    navigationOptions: {
+            tabBarIcon: ({ focused, tintColor }) => {
+                const iconName = `ios-settings${focused ? '' : '-outline'}`;
+                return <Ionicons name={iconName} size={25} color={tintColor} />;
+            },
+        },
+    },
+});
 
-  render() {
-    return (
-      // <MapView
-      //   region = {this.state.region}
-      //   onRegionChange = {this.onRegionChange}
-      //   style={{ flex: 1 }}
-      // >
-      // {this.state.markers.map(marker => (
-      //   <Marker
-      //     coordinate={marker.latlng}
-      //     title={marker.title}
-      //     description={marker.description}
-      //   />
-      // ))}
-      < Map />
-    );
-  }
-
-}
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+export default createAppContainer(TabNavigator);
---
+++ components/Login.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+    Button,
+    StyleSheet,
+    View,
+    Text,
+    Dimensions,
+    TouchableOpacity,
+    Modal,
+    DatePickerIOS,
+    TextInput,
+    Picker
+} from 'react-native';
+
+export default class Login extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+    }
+    render() {
+      return(
+        <Text> Derp </Text>
+      )}
+}
